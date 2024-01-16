9e8d0a75ab8f14f46c2d53322e06697775312928
Federico81 <federico.deambrosis@gmail.com>
2024-01-16 12:49:48 +0100

    Update App.js

--- src-3/App.js
+++ src-3/App.js
@@ -19,3 +19,21 @@ function App() {
 }
 
 export default App;
+
+/*
+Persistore -> npm install redux-persist
+
+Redux Persist è una libreria per la gestione dello stato in applicazioni React che usano Redux come gestore dello state
+-> la sua funzione principale è consentire la persistenza dei dati quando ricarico la pagina o riavvio la applicazione
+Come funziona?
+1. Redux persist consente di mantenere lo stato di Redux anche quando l'appplicazione viene chiusa/riavviata
+2. Storage: uso il meccanismo di storage (Può essere localStorage, sessionStorage, AsyncStorage) per salvare i dati per un ripristino
+3. Configurazione: Andrà configurato dentro a Redux -> si crea un Persistore che contiene il nostro applicativo e specifica alcune opzioni:
+- tipo di storage
+- le key dello stato che deve persitere
+- eventuali trasformazioni
+
+Utilizzo pratico Persistore:
+Redux persiste fornisce un componente dedicato si chiama PersistGate che è quello che viene usato per garantire il caricamento persistente dello stato
+
+*/
