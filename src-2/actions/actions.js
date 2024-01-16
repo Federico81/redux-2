79be4c4708441e0f57c4ab29bf15a7beea28d98e
Federico81 <federico.deambrosis@gmail.com>
2024-01-16 10:41:22 +0100

    Delete src-2 directory

--- src-2/actions/actions.js
+++
@@ -1,5 +0,0 @@
-/*
-Questa parte ormai dovrebbe essere famigliare, vengono definite le actions
-*/
-export const increment = () => ({ type: 'INCREMENT' });
-export const decrement = () => ({ type: 'DECREMENT' });
